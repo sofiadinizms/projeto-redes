6ecbd7c0936a85845ee9671c321eca8604cbb2d9
Agso7 <99735579+Agso7@users.noreply.github.com>
2022-10-27 17:23:13 -0300

    Add files via upload

---
+++ Projeto2/clienteProjeto2.py
@@ -0,0 +1,22 @@
+from socket import socket, AF_INET, SOCK_STREAM
+
+socket_cliente = socket(AF_INET, SOCK_STREAM)
+
+socket_cliente.connect(('192.168.1.101', 8080))
+
+mensagem = ""
+mensagem += 'GET / HTTP/1.1\r\n'
+mensagem += 'Host: 192.168.1.101:8080\r\n'
+mensagem += 'Connection: close\r\n'
+mensagem += 'Accept-Language: pt-BR,pt;q=0.9\r\n'
+mensagem += '\r\n'
+
+
+socket_cliente.send(mensagem.encode())
+
+dados = socket_cliente.recv(2048)
+
+mensagem = dados.decode()
+
+print('Resposta recebida: ')
+print(f'{mensagem}')
